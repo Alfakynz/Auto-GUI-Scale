07c385447313be93a56f4780b4c3ad9ea930b9db
Alfakynz Nyxaeris <141228129+Alfakynz@users.noreply.github.com>
2026-08-13 14:59:27 +0200

    Fix not rounding the value

--- src/main/java/com/alfakynz/autoguiscale/mixin/GuiScaleMixin.java
+++ src/main/java/com/alfakynz/autoguiscale/mixin/GuiScaleMixin.java
@@ -20,7 +20,7 @@ public class GuiScaleMixin {
         Options options = Minecraft.getInstance().options;
         if (options.guiScale().get() == 0) {
             int computedScale = cir.getReturnValue();
-            cir.setReturnValue((int) Math.max(Config.MINIMUM.value, computedScale / Config.DIVIDED.value) - Config.REDUCED.value);
+            cir.setReturnValue(Math.round((float) Math.max(Config.MINIMUM.value, computedScale / Config.DIVIDED.value) - Config.REDUCED.value));
             if (Config.DEBUG.value) AutoGuiScale.LOGGER.info("Adjusted GUI scale from {} to {}", computedScale, cir.getReturnValue());
         }
     }
